ed63b1153757a5398f8c9dc594c05d602c8c828b
eugenius <eudriku@gmail.com>
2018-12-30 23:23:00 +0300

    Kabwama's nonsense

--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
+import json
+import requests
 from django.shortcuts import render
 from django.views.generic import View
-from .forms import UploadFileForm
 from django.http import HttpResponse
 from . import gridExtraction 
+
+request_url = "http://18.191.62.27/api/request"
 
 # Create your views here.
 class CharacterExtractor(View):
@@ -10,14 +13,29 @@ class CharacterExtractor(View):
         return render(request, template_name="image_upload.html")
 
     def post(self, request):
-        form = UploadFileForm(request.POST, request.FILES)
-        # if form.is_valid():
-        with open('uploads/image.png', 'wb+') as destination:
-            for chunk in request.FILES['imagename']:
-                destination.write(chunk)
-        
-        headerdict, resultlist = gridExtraction.imageCharacterExtracter('uploads/image.png')              
-        #resultlist = [{"name": "Kabwama Alvin", "number": 999, "location": "Novia"}]
-        return render(request, template_name="data_view.html", context={"resultlist": resultlist, 'headerdict': headerdict})
-        # else:
-        #     return HttpResponse("Not so good!")
+        resp = requests.get(request_url).json()
+
+        status = resp.get("status")
+        if status == 200:
+            image_id = resp.get("list").get("ID")
+            image_url = resp.get("list").get("image")
+
+            with open('uploads/image.png', 'wb+') as image_file:
+                image_file.write(requests.get(image_url).content)
+
+            headerdict, resultlist = gridExtraction.imageCharacterExtracter('uploads/image.png')
+
+            send_data = {'resultList': resultlist,
+                         'headerDict': headerdict,
+                         'id': image_id}
+
+            requests.post(request_url, data=send_data)
+
+            # else:
+            #     return HttpResponse("Not so good!")
+
+        elif status == 204:
+            pass
+
+        else:
+            pass
